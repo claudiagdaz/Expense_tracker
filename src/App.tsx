749f8ebf7a980e35c962f6a2fbcc24781b057b1a
Claudia Davila <claudiagdaz@gmail.com>
2024-02-26 19:08:29 -0600

    Basic structure for Expenses Form
    No functionality yet but you can see how it's going to look in the page.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ExpenseList from "./components/ExpenseList";
 function App() {
   return (
     <>
+      <h1 className='heading'>Expense Tracker</h1>
       <div className='mb-5'>
         <ExpenseForm />
       </div>
